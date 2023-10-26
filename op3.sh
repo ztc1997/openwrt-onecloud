b1775fe5293bec89d763b619354e71e036345f11
shiyu1314 <1052672227@qq.com>
2023-10-26 21:10:32 +0800

    meson: add Thunder OneCloud Specifications:
    SoC: Amlogic S805 ARM Cortex-A53 1.5 GHz
    RAM: 1 GB RAM
    Flash: 8 GB eMMC
    LAN: 1x RTL6211F Gigabit Ethernet/at support
    LEDs: 1x blue
    USB：Two USB 2.0
    Buttons: 1x reset button
    
    Make it into a direct brush package, making it convenient for software to brush into EMMC without the need for SD or USB drive
    
    u-boot:
    https://github.com/shiyu1314/u-boot-onecloud/releases
    
    SD firmware conversion to direct flash firmware:
    https://github.com/shiyu1314/openwrt-onecloud
    
    Signed-off-by: XueLi <1052672227@qq.com>

---
+++ op3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#=================================================
+sudo rm -rf feeds/luci/applications/luci-app-openclash
+sudo rm -rf feeds/luci/themes/luci-theme-argon
+sudo rm -rf feeds/packages/net/mosdns
+sudo rm -rf feeds/packages/net/adguardhome
+
